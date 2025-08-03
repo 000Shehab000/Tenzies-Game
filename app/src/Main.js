9ee3676f0fcd63afa33340f943565b0844583884
000Shehab000 <shhab200025@gmail.com>
2025-08-03 15:12:09 +0300

    adding random dice

--- app/src/Main.js
+++ app/src/Main.js
@@ -3,21 +3,24 @@ import './style/App.css'
 import Die from './components/Die'
 
 export default function Main() {
+  const [newDice, setNewDice] = React.useState(getAllNewDice())
+
+  function getAllNewDice() {
+    let allNewDice = []
+    for (let i = 0; i < 10; i++) {
+      allNewDice.push(Math.ceil(Math.random() * 6))
+    }
+    return allNewDice
+  }
+
+  const diceElements = newDice.map((number, index) => (
+    <Die key={index} value={number} />
+  ))
+
   return (
     <div className="container">
       <div className="board">
-        <div className="die-container">
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={5} />
-        </div>
+        <div className="die-container">{diceElements}</div>
       </div>
     </div>
   )
